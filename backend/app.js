faf325c8df8c55e871cf744cf66883ca8635c2ac
Mrleonov2 <mikhail-leonov2002@yandex.ru>
2022-08-16 12:56:14 +0700

    fix dom adress2

--- backend/app.js
+++ backend/app.js
@@ -20,12 +20,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 //
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(cookieParser());
