0c8b4c1ebfb1f6e873e1aa91671bc9f3d42d72bd
Mrleonov2 <mikhail-leonov2002@yandex.ru>
2022-08-16 22:53:34 +0700

    added bodyPars

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const { celebrate, Joi, errors } = require('celebrate');
 
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const corsOption = require('./utils/corsOption')
 const helmet = require('helmet');
+const corsOption = require('./utils/corsOption');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
@@ -19,14 +20,15 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-//
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOption));
 app.use(helmet());
-// app.use(requestLogger);
+app.use(requestLogger);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -34,29 +36,39 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(new RegExp(regExp)),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(new RegExp(regExp)),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
   }),
-}), createUser);
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+  createUser,
+);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
   }),
-}), login);
+  login,
+);
 app.get('/signout', (_, res) => {
   res.status(200).clearCookie('jwt').send({ message: 'Выход' });
 });
 app.use(auth, routerUser);
 app.use(auth, routerCard);
-app.use('*', auth, (req, res, next) => { next(new NotFoundError('Страница по указанному URL не найдена')); });
-// app.use(errorLogger);
-// app.use(errors());
+app.use('*', auth, (req, res, next) => {
+  next(new NotFoundError('Страница по указанному URL не найдена'));
+});
+app.use(errorLogger);
+app.use(errors());
 
 app.use(errHandler);
 app.listen(PORT, () => {
--- backend/utils/corsOption.js
+++ backend/utils/corsOption.js
@@ -1,9 +1,11 @@
 const corsOption = {
-  origin:[
+  origin: [
     'https://api.leonov.nomoredomains.sbs',
     'http://api.leonov.nomoredomains.sbs',
-    'http:/localhost:3000'
+    'http:/localhost:3000',
   ],
-  credentials:true
+  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Origin', 'Autorization'],
 };
 module.exports = corsOption;
